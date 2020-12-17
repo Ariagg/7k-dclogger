a2c827ee45ef3db866ed1fbbe35445f37b078499
7k <58889981+Ariagg@users.noreply.github.com>
2020-12-17 10:33:58 +0700

    Updated

--- status.lua
+++ status.lua
@@ -1,13 +1,13 @@
 Citizen.CreateThread(function()
     while true do
-		SetDiscordAppId(785253255374307338)
-		SetDiscordRichPresenceAsset('indovice')
+		SetDiscordAppId("Place Your App ID")
+		SetDiscordRichPresenceAsset('your-big-picture-asset-name')
         local playerName = GetPlayerName(PlayerPedId(-1))
         SetDiscordRichPresenceAssetText(string.format("%s", playerName))
 		SetDiscordRichPresenceAssetSmall('darah')
 		--local darah = GetEntityHealth(GetPlayerPed(-1))
-		SetDiscordRichPresenceAssetSmallText(string.format("discord.st/IndoVice"))
-		local maxPlayerSlots = "50"
+		SetDiscordRichPresenceAssetSmallText(string.format("7k-resource"))
+		local maxPlayerSlots = "69"
 		local playerCount = #GetActivePlayers()
         SetRichPresence(string.format("%s/%s Warga", playerCount, maxPlayerSlots))
 		Citizen.Wait(60000)
