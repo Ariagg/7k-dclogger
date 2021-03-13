13ac525987b04798fffa4880bcbbf4f6b48ce1bc
7k <58889981+Ariagg@users.noreply.github.com>
2021-03-13 18:13:25 +0700

    This Is Rich Presence

--- status.lua
+++ status.lua
@@ -1,15 +1,15 @@
 Citizen.CreateThread(function()
     while true do
-		SetDiscordAppId("Place Your App ID")
-		SetDiscordRichPresenceAsset('your-big-picture-asset-name')
+	SetDiscordAppId("Place Your App ID")
+	SetDiscordRichPresenceAsset('your-big-picture-asset-name')
         local playerName = GetPlayerName(PlayerPedId(-1))
         SetDiscordRichPresenceAssetText(string.format("%s", playerName))
-		SetDiscordRichPresenceAssetSmall('darah')
-		--local darah = GetEntityHealth(GetPlayerPed(-1))
-		SetDiscordRichPresenceAssetSmallText(string.format("7k-resource"))
-		local maxPlayerSlots = "69"
-		local playerCount = #GetActivePlayers()
+	SetDiscordRichPresenceAssetSmall('darah')
+	--local darah = GetEntityHealth(GetPlayerPed(-1))
+	SetDiscordRichPresenceAssetSmallText(string.format("7k-resource"))
+	local maxPlayerSlots = "69"
+	local playerCount = #GetActivePlayers()
         SetRichPresence(string.format("%s/%s Warga", playerCount, maxPlayerSlots))
-		Citizen.Wait(60000)
+	Citizen.Wait(60000)
 	end
 end)
